c38f394d0e4c2f59986af4ea31f36ee9c673ea1d
tugi-ngunjiri <blueqwinn@gmail.com>
2022-11-22 14:08:56 +0300

    done with assigment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+describe('index.js', function () {
+    // there's stuff in here, too
+    it('is set as Scuber', function () {
+        // tests are here
+      });  
+
+let companyName = 'Scuber';
