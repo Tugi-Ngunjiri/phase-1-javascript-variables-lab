1f048ab191ae65b3f73fa5caa41578298aaad2f1
tugi-ngunjiri <blueqwinn@gmail.com>
2022-11-30 08:29:34 +0300

    done with assigment

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-// Code your solution in this file!
-describe('index.js', function () {
-    // there's stuff in here, too
-    it('is set as Scuber', function () {
-        // tests are here
-      });  
-
-let companyName = 'Scuber';
+const companyName = 'Scuber';
+let mostProfitableNeighborhood = 'Chelsea'
+let companyCeo  = 'Susan Smith'
